0789b5efa2fa1d200b3e1fb1066d6ee8d0352f16
Fabien Marteau <mail@fabienm.eu>
2023-10-24 22:11:03 +0200

    trying to refresh qrcode image

--- qr-note/src/main.rs
+++ qr-note/src/main.rs
@@ -1,16 +1,9 @@
 use qrcode::QrCode;
 use image::Luma;
+use std::path::Path;
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
-
-    let mut count = 0;
-
-    /* QrCode tests */
-    // Save the image.
-//    image.save("qrcode.png").unwrap();
-    /* end of QrCode tests */
-
     let ui = AppWindow::new()?;
 
     let ui_handle = ui.as_weak();
@@ -19,20 +12,19 @@ fn main() -> Result<(), slint::PlatformError> {
         ui.set_counter(ui.get_counter() + 1);
         println!("button pushed");
     });
-
+    let mut count = 0;
     let ui_handle = ui.as_weak();
     ui.on_compute_qr_code(move || {
         let ui = ui_handle.unwrap();
-        println!("{}",ui.get_thetext());
-    });
-
-    let ui_handle = ui.as_weak();
-    ui.on_qr_note_edited(move || {
-        let ui = ui_handle.unwrap();
-//        println!("text changed -> {}", ui.get_thetext());
         let code = QrCode::new(ui.get_thetext()).unwrap();
         let image = code.render::<Luma<u8>>().build();
-        image.save("qrcode.png").unwrap();
+//        fs::remove_file("ui/icons/qrcode.png").unwrap();
+        image.save("ui/icons/qrcode.png").unwrap();
+        use slint::Image;
+        let qrimage = Image::load_from_path(Path::new("ui/icons/qrcode.png")).unwrap();
+        ui.set_qrnote(qrimage);
+        println!("count {}", count);
+        count = count + 1;
     });
 
     ui.run()
